456ec07d7cd501530e7b3232c5417768fc33c5f8
Pamela Anderson <pamelaanderson519@yahoo.com>
2018-11-27 19:41:56 -0500

    bug fix

--- models/Kudos.js
+++ models/Kudos.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-/**
- * Create a new Task Schema to map Mongo documents to an object in our node application
- */
+
 var KudosSchema = new Schema({
   title: String,
   body: String,
